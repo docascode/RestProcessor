d38b688faf3812acec3c2b0e786b1e9cb42280ed
Huang Xueyuan(Jessie) <hxyjessie@gmail.com>
2018-04-09 11:54:23 +0800

    Use ContactDescription

--- Microsoft.RestApi.RestTransformer/Utility.cs
+++ Microsoft.RestApi.RestTransformer/Utility.cs
@@ -132,7 +132,7 @@
             var content = summary;
             if (!string.IsNullOrEmpty(description) && summary != description)
             {
-                content = string.IsNullOrEmpty(summary) ? description : $"{summary} <br/> {description}";
+                content = string.IsNullOrEmpty(summary) ? description : ContactDescription(summary, description);
             }
             return content;
         }
