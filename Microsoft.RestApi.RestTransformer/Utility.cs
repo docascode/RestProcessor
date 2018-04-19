f8c94bcbcb558d24bd8a5b77d104b096dc10ef23
partychen <partychen.acm@gmail.com>
2018-04-19 13:19:39 +0800

    update the status code

--- Microsoft.RestApi.RestTransformer/Utility.cs
+++ Microsoft.RestApi.RestTransformer/Utility.cs
@@ -198,6 +198,14 @@
                     return "204 No Content";
                 case "400":
                     return "400 Bad Request";
+                case "401":
+                    return "401 Unauthorized";
+                case "403":
+                    return "403 Forbidden";
+                case "404":
+                    return "404 Not Found";
+                case "500":
+                    return "500 Internal Server Error";
                 default:
                     return "Other Status Codes";
             }
