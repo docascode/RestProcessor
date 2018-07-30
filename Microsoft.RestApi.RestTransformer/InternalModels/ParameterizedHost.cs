9adac7e4b6402853a0feaa679b6175e00badb03f
Zikun Fan <zifan@microsoft.com>
2018-07-30 15:39:48 +0800

    [32008]Add useSchemePrefix checking

---
+++ Microsoft.RestApi.RestTransformer/InternalModels/ParameterizedHost.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.RestApi.RestTransformer.Models;
+using System.Collections.Generic;
+
+namespace Microsoft.RestApi.RestTransformer
+{
+    public class ParameterizedHost
+    {
+        public string Host { get; set; }
+
+        public bool UseSchemePrefix { get; set; }
+
+        public List<ParameterEntity> Parameters { get; set; }
+    }
+}
