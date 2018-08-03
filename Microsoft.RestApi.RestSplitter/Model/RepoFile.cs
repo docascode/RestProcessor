ee37e0b5272527d8e4d2e190729917f53b3f16c6
partychen <partychen.acm@gmail.com>
2018-08-03 16:54:39 +0800

    add source url

---
+++ Microsoft.RestApi.RestSplitter/Model/RepoFile.cs
@@ -0,0 +1,25 @@
+﻿namespace Microsoft.RestApi.RestSplitter.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    [Serializable]
+    public class RepoFile
+    {
+        [JsonProperty("repo")]
+        public IList<RepoConfig> Repos { get; set; }
+    }
+
+    public class RepoConfig
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("branch")]
+        public string Branch { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
