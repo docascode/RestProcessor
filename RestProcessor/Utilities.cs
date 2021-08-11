59b9a3558023ff3e530403a84b81c47a4f529747
v-haiboz <v-haiboz@microsoft.com>
2021-08-11 17:50:13 +0800

    Updated

--- RestProcessor/Utilities.cs
+++ RestProcessor/Utilities.cs
@@ -16,21 +16,21 @@ namespace RestProcessorUtilities
             var result = new List<string>();
             foreach (var orgInfo in mappingFile.OrgInfos)
             {
-                if (orgInfo == null)
+                if (orgInfo == null || orgInfo.Services == null)
                 {
                     continue;
                 }
 
                 foreach (var service in orgInfo.Services)
                 {
-                    if (service == null)
+                    if (service == null || service.SwaggerInfo == null)
                     {
                         continue;
                     }
 
                     foreach (var swagger in service.SwaggerInfo)
                     {
-                        if (swagger.Source != null)
+                        if (swagger == null || swagger.Source != null)
                         {
                             result.Add(Path.Combine(rootPath, swagger.Source));
                         }
